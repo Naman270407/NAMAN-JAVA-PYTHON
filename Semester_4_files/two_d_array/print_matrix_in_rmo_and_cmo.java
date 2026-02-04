aece1d8c17b4c34e89bb84de0c7ec7ab543bffd4
Naman270407 <namanmaheshwari422@gmail.com>
2026-02-04 09:11:05 +0530

    your message

---
+++ Semester_4_files/two_d_array/print_matrix_in_rmo_and_cmo.java
@@ -0,0 +1,45 @@
+package Semester_4_files.two_d_array;
+
+import java.util.Scanner;
+
+public class print_matrix_in_rmo_and_cmo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rows: ");
+        int r = sc.nextInt();
+        System.out.print("Enter columns: ");
+        int c = sc.nextInt();
+
+        int[][] a = new int[r][c];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matrix is:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Row major order : ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Column major order : ");
+        for (int j = 0; j < c; j++){
+            for (int i = 0; i < r; i++){
+                System.out.print(a[i][j] + " ");
+            }
+        }
+    }
+
+}
