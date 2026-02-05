41165886fe83dead2ca575e805c2ebf2a007f552
Naman270407 <namanmaheshwari422@gmail.com>
2026-02-05 12:47:15 +0530

    core elements / mid elements of array

---
+++ Semester_4_files/two_d_array/print_core_elements.java
@@ -0,0 +1,36 @@
+package Semester_4_files.two_d_array;
+
+import java.util.Scanner;
+
+public class print_core_elements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] a = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Print Core elements : ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1) {
+                    System.out.print("\t");
+                } else {
+                    System.out.print(a[i][j] + "\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Semester_4_files/two_d_array/print_frame_element.java
@@ -0,0 +1,40 @@
+package Semester_4_files.two_d_array;
+
+import java.util.Scanner;
+
+public class print_frame_element {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] a = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // print frame elements
+        System.out.println("\nFrame Elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1) {
+                    System.out.print(a[i][j] + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
